f2a93775a27ef479ce33186c2f6ce31231524d3f
VegaCristian-FS <cavega@student.fullsail.edu>
2022-02-13 00:05:16 -0500

    Initial app build

---
+++ Code/movie-app/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Header/>
+      </header>
+      <main>
+        <section>
+          {/* nav links to other pages */}
+        </section>
+        <section>
+          {/* Data printed out */}
+        </section>
+      </main>
+      <footer>
+        {/* black bar/ copyright? */}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ Code/movie-app/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {ReactComponent as Logo} from '../assets/logo.svg';
+
+function Header() {
+    return (
+        <nav>
+            <div>
+                <h1> <Logo/> </h1>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Header
