73b4915734dcefa85c33f2cc2d94ff0f360aaa00
VegaCristian-FS <cavega@student.fullsail.edu>
2022-02-27 18:23:49 -0500

    Edit overlay settings

--- Code/movie-app/src/components/MovieCard.js
+++ Code/movie-app/src/components/MovieCard.js
@@ -7,7 +7,7 @@ function MovieCard(params) {
                 <img src={'https://image.tmdb.org/t/p/original'+params.info.poster_path} alt={params.info.original_title + ' poster'}></img>
                 <div className="movie-overlay">
                     <h4>{params.info.title} - {params.info.release_date.substring(0,4)}</h4>
-                    <p>{params.info.overview}</p>
+                    <p>{params.info.overview.substring(0,260)}...</p>
                 </div>
             </article>
         </div> 
