0b98d5faed6537a72e755c8c1c324ce31dedde13
VegaCristian-FS <cavega@student.fullsail.edu>
2022-02-27 17:31:49 -0500

    Testing changes

--- Code/movie-app/src/components/MovieCard.js
+++ Code/movie-app/src/components/MovieCard.js
@@ -6,7 +6,8 @@ function MovieCard(params) {
             <article className="movie">
                 <img src={'https://image.tmdb.org/t/p/original'+params.info.poster_path} alt={params.info.original_title + ' poster'}></img>
                 <div className="movie-overlay">
-                    <h4>{params.info.title}</h4>
+                    <h4>{params.info.title} - {params.info.release_date.substring(0,4)}</h4>
+                    <p>{params.info.overview}</p>
                 </div>
             </article>
         </div> 
