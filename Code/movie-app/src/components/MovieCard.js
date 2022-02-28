0a92d89acf6a08a7b21a8f84e6b39c4fc4901ea0
VegaCristian-FS <cavega@student.fullsail.edu>
2022-02-27 19:07:20 -0500

    Adjusting Poster img

--- Code/movie-app/src/components/MovieCard.js
+++ Code/movie-app/src/components/MovieCard.js
@@ -4,7 +4,7 @@ function MovieCard(params) {
     return (    
         <div>   
             <article className="movie">
-                <img src={'https://image.tmdb.org/t/p/original'+params.info.poster_path} alt={params.info.original_title + ' poster'}></img>
+                <img src={'https://image.tmdb.org/t/p/w185'+params.info.poster_path} alt={params.info.original_title + ' poster'}></img>
                 <div className="movie-overlay">
                     <h4>{params.info.title} - {params.info.release_date.substring(0,4)}</h4>
                     <p>{params.info.overview.substring(0,260)}...</p>
